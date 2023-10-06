b8279a8aba82994e18c35ad1536fecec50f324fe
Pierre-Yves Landuré <pierre-yves@landure.fr>
2023-10-06 06:22:05 +0200

    build: :wrench: add tailwind nesting plugin to PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,28 @@
 // postcss.config.js
 const themeDir = __dirname;
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
-    content: [
-        './hugo_stats.json',
-        themeDir + '/hugo_stats.json',
-        'exampleSite/hugo_stats.json',
-    ],
-    safelist : [ /type/ ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
-})
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
+  content: [
+    "./hugo_stats.json",
+    themeDir + "/hugo_stats.json",
+    "exampleSite/hugo_stats.json",
+  ],
+  safelist: [/type/],
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
+  },
+});
 
 module.exports = {
   plugins: [
-    require('postcss-import')({root: themeDir,}),
-    require('tailwindcss')(themeDir + 'tailwind.config.js'),
-    require('autoprefixer')({
-      path: [themeDir]
+    require("postcss-import")({ path: [themeDir] }),
+    require("tailwindcss/nesting"),
+    require("tailwindcss")(themeDir + "/tailwind.config.js"),
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+    require("autoprefixer")({
+      path: [themeDir],
     }),
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
-  ]
-}
+  ],
+};

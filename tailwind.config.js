fe450e549e464b85700e0cee00d69c252e635a25
Pierre-Yves Landuré <pierre-yves@landure.fr>
2023-10-15 20:04:14 +0200

    fix(tailwindcss): :wrench: fix daisyUI outputing log in CSS, and add theme dir to tailwind CSS content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const themeDir = __dirname;
+
 module.exports = {
   darkMode: "class",
   content: [
@@ -6,6 +9,7 @@ module.exports = {
     "./content/**/layouts/**/*.html",
     "./layouts/**/*.html",
     "./content/**/*.html",
+    themeDir + "/layouts/**/*.html",
   ],
   theme: {
     extend: {},
@@ -25,4 +29,14 @@ module.exports = {
     // require('@tailwindcss/aspect-ratio'),
     // require('@tailwindcss/line-clamp'),
   ],
+  daisyui: {
+    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: false, // Disable logs output, since it's store in final CSS file and breaks the result.
+  },
 };
